f611eacf515da6e420eb7b9cd263bac656dc3318
Dsp29 <st.david29@hotmail.com>
2018-01-15 17:43:07 -0500

    second commit

--- src/firstClass.java
+++ src/firstClass.java
@@ -1,9 +1,35 @@
+/**
+ * 
+ */
 
+import java.util.*;
 public class firstClass {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+//		String s;
+		int i = 5;
+//		Scanner a = new Scanner(System.in);
+//		System.out.println("Give a string" );
+//		s = a.nextLine();
+//		System.out.println("\n String is " + s);
+//		
+//		//for(int i=0;i<5;i++) {
+//		//	System.out.println("\n String is Number " + i);
+//			
+//	//	}
+		
+		if (i==5) {
+			System.out.println("\n i is 5");
+			
+		}
+		else {
+			System.out.println("hello " + i);
+			
+		}
 	}
 
 }
